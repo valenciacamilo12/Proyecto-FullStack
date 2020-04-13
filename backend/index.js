a4697490a818f967e67582a7b74c311e59bdc356
Camilo Andres Valencia Ramirez <camilo3341215@gmail.com>
2020-04-13 18:39:47 -0500

    Configuracion de proyecto inicial

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+// Initializations
+const app = express();
+
+// Settings
+app.set ('port', 3000);
+
+
+// Start the server
+app.listen(app.get('port'), () => {
+    console.log("Server on port", app.get('port'));
+})
